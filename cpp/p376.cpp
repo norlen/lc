21c2006bd314e1f5767c9992f422c3a6f72ab7d8
norlen <joacim.norlen@gmail.com>
2021-10-16 08:41:36 +0200

    +

---
+++ cpp/p376.cpp
@@ -0,0 +1,21 @@
+// [376. Wiggle Subsequence](https://leetcode.com/problems/wiggle-subsequence/)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int up = 1, down = 1;
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i-1] < nums[i]) {
+                up = down + 1;
+            } else if (nums[i-1] > nums[i]) {
+                down = up + 1;
+            }
+        }
+        return max(up, down);
+    }
+};
